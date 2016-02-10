e84a627048c827b73f9136f8f9a7518eb214d1e3
Priyanka <priyanka.sabhagani@bookmyshow.com>
2016-02-10 19:33:48 +0530

    listview on first pg

--- app/src/main/java/com/example/priyankasabhagani/assessment1/MainActivity.java
+++ app/src/main/java/com/example/priyankasabhagani/assessment1/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.priyankasabhagani.assessment1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        String[] arrMainActivityList = {"one","two","three"};
+        ArrayAdapter adapterMainActivity = new ArrayAdapter(this,R.layout.activity_main,arrMainActivityList);
+        ListView listView = (ListView) findViewById(R.id.main_activity_list);
+        listView.setAdapter(adapterMainActivity);
         setContentView(R.layout.activity_main);
     }
 }
