1800dff715a0d2ea502c414aa69ecb63f05220ed
Priyanka <priyanka.sabhagani@bookmyshow.com>
2016-02-10 13:10:26 +0530

    feature:initial splash screen

---
+++ app/src/main/java/com/example/priyankasabhagani/assessment1/SplashScreen.java
@@ -0,0 +1,36 @@
+//package com.coderefer.androidsplashscreenexample;
+package com.example.priyankasabhagani.assessment1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by priyanka.sabhagani on 2/9/2016.
+ */
+public class SplashScreen extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
